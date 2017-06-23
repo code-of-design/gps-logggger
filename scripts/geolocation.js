d3ba9116ddc8b2ccab3ec3bb1f5fbe7360d99f7c
Aiba Akihito <hash.codeofdesign@gmail.com>
2017-06-23 17:27:04 +0900

    watchPosition()の取得

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -3,71 +3,101 @@
 (function($){
   "use strict";
 
-  var latitude;
-  var longitude;
+  var latitude = 0.0; // 緯度.
+  var longitude = 0.0; // 経度.
+  var latitude_log = []; // 緯度ログ.
+  var longitude_log = []; // 経度ログ.
+  var lapsed_time = {
+    hour: 0,
+    minute: 0,
+    second: 0
+  }; // 経過時間.
+  var distance = 0; // 移動距離.
+  var distance_log = []; // 移動距離ログ.
+
   var latitude_id = document.getElementById("latitude");
   var longitude_id = document.getElementById("longitude");
   var minimap_id = document.getElementById("map");
   var current_time_id = document.getElementById("current-time__time");
+  var lapsed_time_id = document.getElementById("lapsed-time__time");
 
+  // SET UP
   window.onload = function(){
-    // 現在時刻を取得する.
-    // setInterval(getCurrentTime, 1000);
-    // 位置情報を監視する.
-    watchPosition();
+    // 現在位置を取得する
+    getCurrentPosition();
   };
 
-  // 現在位置を取得する.
+  // 現在位置を取得する
   function getCurrentPosition() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(function (position) {
-        // 緯度経度を取得する.
+        // 緯度経度を取得する
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
-        // 緯度経度を正規化する.
+        // 緯度経度を正規化する
         latitude = parseFloat(latitude.toFixed(3));
         longitude = parseFloat(longitude.toFixed(3));
-        // Google Mapsを表示する.
-        viewMinimap(latitude, longitude);
-        // 現在位置を表示する.
+        // 現在位置を表示する
         viewCurrentPosition(latitude_id, longitude_id,latitude, longitude);
+        // Google Mapsを表示する
+        viewMinimap(latitude, longitude);
       }, errorGetPosition); // 位置情報取得のエラーハンドリング.
     }
   }
 
-  // 位置情報を監視する.
+  // 位置情報を監視する
   function watchPosition() {
     if ("geolocation" in navigator) {
       navigator.geolocation.watchPosition(function (position) {
-        // 緯度経度.
+        // 緯度経度を取得する
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-        // 緯度経度を正規化する.
+        // 緯度経度を正規化する
         latitude = parseFloat(latitude.toFixed(3));
         longitude = parseFloat(longitude.toFixed(3));
-        // Google Mapsを表示する.
-        // viewMinimap(latitude, longitude);
-        // 現在位置を表示する.
+        // 現在位置を表示する
         viewCurrentPosition(latitude_id, longitude_id, latitude, longitude);
-        // 位置情報を記録する.
-        clickLogbtn(latitude, longitude);
+        // 緯度経度をログする.
+        if (latitude_log.length > 0) {
+          if (latitude_log[latitude_log.length] >= (latitude+0.05) ||
+          latitude_log[latitude_log.length] <= (latitude-0.05)) {
+            latitude_log.push(latitude);
+            // Google Mapsを表示する
+            viewMinimap(latitude, longitude);
+          }
+        }
+        else{
+          latitude_log.push(latitude);
+        }
+        if (longitude_log.length > 0) {
+          if (longitude_log[longitude_log.length] >= (longitude+0.05) ||
+          longitude_log[longitude_log.length] <= (longitude-0.05)) {
+            longitude_log.push(longitude);
+            // Google Mapsを表示する
+            viewMinimap(latitude, longitude);
+          }
+        }
+        else {
+          longitude_log.push(longitude);
+        }
+        console.log(latitude_log, longitude_log);
       }, errorGetPosition);
     }
   }
 
-  // 位置情報取得のエラーハンドリング.
-  function errorGetPosition(error) {
-    console.log(error.code);
-    console.log(error.message);
-  }
-
-  // 現在位置を表示する.
+  // 現在位置を表示する
   function viewCurrentPosition(latitude_id, longitude_id, latitude, longitude) {
     latitude_id.innerHTML = "LAT: " + latitude;
     longitude_id.innerHTML = "LNG: " + longitude;
   }
 
-  // 現在時刻を取得する.
+  // 位置情報取得のエラーハンドリング
+  function errorGetPosition(error) {
+    console.log(error.code);
+    console.log(error.message);
+  }
+
+  // 現在時刻を取得する
   function getCurrentTime(){
     var current_time = new Date();
     var year = current_time.getFullYear();
@@ -77,34 +107,60 @@
     var minute = current_time.getMinutes();
     var second = current_time.getSeconds();
     var time = hour+":"+minute+":"+second;
-    // 現在時刻を表示する.
+    // 現在時刻を表示する
     viewCurrentTime(time);
   }
 
-  // 現在時刻を表示する.
+  // 現在時刻を表示する
   function viewCurrentTime(time){
     current_time_id.innerHTML = time;
   }
 
-  // Google Mapsを表示する.
+  // 経過時間を取得する
+  function getLapsedTime(){
+    if (lapsed_time.second < 59) {
+      lapsed_time.second += 1;
+    }
+    else {
+      lapsed_time.second = 0;
+      lapsed_time.minute += 1;
+    }
+
+    if (lapsed_time.minute > 59) {
+      lapsed_time.minute = 0;
+      lapsed_time.hour += 1;
+    }
+    viewLapsedTime(lapsed_time.hour, lapsed_time.minute, lapsed_time.second);
+  }
+
+  // 経過時間を表示する.
+  function viewLapsedTime(hour, minute, second){
+    var time;
+    time = (hour < 10) ? ("0"+hour+":") : (hour+":");
+    time += (minute < 10) ? ("0"+minute+":") : (minute+":");
+    time += (second < 10) ? ("0"+second) : second
+    lapsed_time_id.innerHTML = time;
+  }
+
+  // Google Mapsを表示する
   function viewMinimap(latitude, longitude) {
     var current_position = {
       lat: latitude,
       lng: longitude
     };
-    // Mapを宣言する.
+    // Mapを宣言する
     var minimap = new google.maps.Map(minimap_id, {
       zoom: 17,
       center: current_position
     });
-    // マーカを表示する.
+    // マーカを表示する
     var marker = new google.maps.Marker({
       position: current_position,
       map: minimap
     });
   }
 
-  // 2点距離を測定する.
+  // 2点距離を測定する
   // https://developers.google.com/maps/documentation/javascript/geometry?hl=ja
   function getBetweenDistance(lat_begin, lng_begin, lat_end, lng_end){
     var begin_position = new google.maps.LatLng(lat_begin, lng_begin);
@@ -114,11 +170,14 @@
     return distance;
   }
 
-  // 位置情報を記録する.
-  function clickLogbtn(latitude, longitude){
+  // 位置情報を記録する
+  //function clickLogbtn(latitude, longitude){
     $(".begin-btn__btn").click(function(){
-      viewMinimap(latitude, longitude);
+      // 経過時間を取得する
+      setInterval(getLapsedTime, 1000);
+      // 位置情報を監視する
+      watchPosition();
     });
-  }
+  //}
 
 })(jQuery);
